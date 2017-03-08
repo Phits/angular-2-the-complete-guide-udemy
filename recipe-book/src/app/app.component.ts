27c48b7dc2a17a968e7828355519fbdf3cbdd4cb
phits <john@fitz.com>
2017-03-08 09:12:04 -0500

    Section 3 completed

---
+++ recipe-book/src/app/app.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+import {} from './header';
+
+@Component({
+  selector: 'rb-root',
+  templateUrl: './app.component.html'
+})
+export class AppComponent {
+  
+}
