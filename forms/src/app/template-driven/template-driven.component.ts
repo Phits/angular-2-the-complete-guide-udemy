fee3e374c154a967dfd01f17c5d932dfaa5b7e6f
phits <john@fitz.com>
2017-03-16 15:20:00 -0400

    Forms continued

---
+++ forms/src/app/template-driven/template-driven.component.ts
@@ -0,0 +1,19 @@
+import { Component } from '@angular/core';
+import { NgForm } from '@angular/forms';
+
+@Component({
+  selector: 'template-driven',
+  templateUrl: 'template-driven.component.html',
+  styles: [`
+    .ng-invalid {
+        border: 2px solid red;
+    } 
+  `]
+})
+export class TemplateDrivenComponent {
+
+    onSubmit(form: NgForm) {
+    	console.log(form);
+    }
+
+}
