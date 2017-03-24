ed62649dd3b51bbc6b40954e07f841c1daf81117
phits <john@fitz.com>
2017-03-24 16:04:00 -0400

    Section 41 completed

---
+++ prj-forms/src/app/recipes/recipe.service.ts
@@ -0,0 +1,84 @@
+import { Injectable, EventEmitter } from '@angular/core';
+import { Headers, Http, Response } from '@angular/http';
+import 'rxjs/add/operator/map'
+
+import { Recipe } from "./recipe";
+import { Ingredient } from "../shared";
+
+@Injectable()
+export class RecipeService {
+  recipesChanged = new EventEmitter<Recipe[]>();
+  
+  private recipes: Recipe[] = [
+    new Recipe('Schnitzel', 'Very tasty', 'https://www.quilzer.net/assets/WienerSchnitzel.jpg', [
+      new Ingredient('French Fries', 2),
+      new Ingredient('Pork Meat', 1)
+    ]),
+    new Recipe('Summer Salad', 'Okayish', 'http://ohmyveggies.com/wp-content/uploads/2013/06/the_perfect_summer_salad.jpg', [])
+  ];
+
+  constructor(private http: Http) {}
+
+  getRecipes() {
+    return this.recipes;
+  }
+
+  getRecipe(id: number) {
+    return this.recipes[id];
+  }
+
+  deleteRecipe(recipe: Recipe) {
+    this.recipes.splice(this.recipes.indexOf(recipe), 1);
+  }
+
+  addRecipe(recipe: Recipe) {
+    this.recipes.push(recipe);
+  }
+
+  editRecipe(oldRecipe: Recipe, newRecipe: Recipe) {
+    this.recipes[this.recipes.indexOf(oldRecipe)] = newRecipe;
+  }
+
+  storeData() {
+     const body = JSON.stringify(this.recipes);
+     const headers = new Headers({
+        'Content-Type': 'application/json'
+     });
+     return this.http.put('https://recipebook-b42b1.firebaseio.com/recipes.json', body, {headers: headers});
+  }
+
+  fetchData() {
+    return this.http.get('https://recipebook-b42b1.firebaseio.com/recipes.json').map((response: Response) => response.json())
+      .subscribe(
+        (data: Recipe[]) => {
+           this.recipes = data;
+           this.recipesChanged.emit(this.recipes);
+        }
+      );
+  }
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
